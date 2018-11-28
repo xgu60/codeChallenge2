b7c313fbdf24795d018f05f744d4fc730bdcd5e9
sheldon <xgu60.gt@gmail.com>
2018-11-27 19:47:45 -0500

    update names

--- maze.py
+++ maze.py
@@ -5,26 +5,15 @@ import sys
 #North (0, 1), South(0, -1), West(-1, 0), East(1, 0)
 
 #Mirror object
-#setup reflection rules based on different symbols
-#rules can be much more complicated as shown
 class Mirror(object):
     def __init__(self, symbol):
         self.symbol = symbol
-        if symbol == '/':
-            self.rule = {(1, 0) : (0, 1),    #East to North
-                         (0, -1) : (-1, 0),  #South to West
-                         (-1, 0) : (0, -1),  #West to South
-                         (0, 1) : (1, 0)}    #North to East
-        elif symbol == '\\':
-            self.rule = {(1, 0) : (0, -1),   #East to South
-                         (0, -1) : (1, 0),   #South to East
-                         (-1, 0) : (0, 1),   #West to North
-                         (0, 1) : (-1, 0)}   #North to West 
-        else:
-            print("please input right symbol for mirror")
-
+        
     def reflect(self, direction):
-        return self.rule[direction]
+        if self.symbol == '/':
+            return direction[1], direction[0]
+        else:
+            return -direction[1], -direction[0]
 
 #Maze object
 class Maze(object):
@@ -33,40 +22,40 @@ class Maze(object):
         self.y = y
         self.mirrors = {}
 
-    def isValidPos(self, pos): #check whether a pos in maze
+    def is_valid_pos(self, pos): #check whether a pos in maze
         if pos[0] < 0 or pos[1] < 0:
             return False
         if pos[0] >= self.x or pos[1] >= self.y:
             return False
         return True
 
-    def addMirror(self, pos, mirror): #add mirror to one position
+    def add_mirror(self, pos, mirror): #add mirror to one position
         self.mirrors[pos] = mirror
 
-    def getMirror(self, pos): #check and get the mirror object at one position
+    def get_mirror(self, pos): #check and get the mirror object at one position
         if pos not in self.mirrors:
             return -1
         return self.mirrors[pos]
 
 #Laser object
 class Laser(object):
-    def __init__(self, startPos, startDir, maze):        
-        self.startPos = startPos
-        self.startDir = startDir
+    def __init__(self, start_pos, start_dir, maze):        
+        self.start_pos = start_pos
+        self.start_dir = start_dir
         self.maze = maze   
         #use set to store states (pos, dir)  
         self.states = set()  
         #use list to store each visited pos    
         self.path = []
             
-    def getPath(self):
-        self.move(self.startPos, self.startDir)
+    def get_path(self):
+        self.move(self.start_pos, self.start_dir)
         return self.path
 
     #recursive call to move the laser
     def move(self, position, direction):
         #check the validity of position
-        if  not self.maze.isValidPos(position):
+        if  not self.maze.is_valid_pos(position):
             self.path.append(-1)
             return
         state = position + direction
@@ -78,7 +67,7 @@ class Laser(object):
         self.path.append(position)
         #update position and direction
         newPos = tuple(i + j for i, j in zip(position, direction))
-        mr = self.maze.getMirror(newPos)
+        mr = self.maze.get_mirror(newPos)
         if mr == -1:
             newDir = direction
         else:
@@ -86,7 +75,7 @@ class Laser(object):
         #recursive call
         self.move(newPos, newDir)
     
-    def printLaserMaze(self):
+    def print_laserMaze(self):
         for x in range(self.maze.x):
             print("---", end="")
         print("\n")
@@ -106,15 +95,14 @@ class Laser(object):
 
 #read a txt file 
 #output a laser object
-def readLaserMaze(filePath):
+def read_laserMaze(file_path):
     directions = {"N": (0, 1),    #North
                   "W" : (-1, 0),  #West
                   "S" : (0, -1),  #South
                   "E" : (1, 0)}   #East
     try:
-        with open(filePath) as fp:
-            line = fp.readline()
-            line = line.strip()
+        with open(file_path) as fp:
+            line = fp.readline().strip()
             cnt = 1
             while line:
                 if cnt == 1:
@@ -125,9 +113,8 @@ def readLaserMaze(filePath):
                     laser = Laser((int(x), int(y)), directions[z], maze)
                 else:
                     x, y, z = line.split(" ")
-                    maze.addMirror((int(x), int(y)), Mirror(z))
-                line = fp.readline()
-                line = line.strip()
+                    maze.add_mirror((int(x), int(y)), Mirror(z))
+                line = fp.readline().strip()
                 cnt += 1
         return laser
 
@@ -135,10 +122,10 @@ def readLaserMaze(filePath):
         fp.close()
 
 #write to a file
-def writeFile(laser, filePath):
-    fp = open(filePath, "w")
-    path = laser.getPath()
-    laser.printLaserMaze()    
+def write_file(laser, file_path):
+    fp = open(file_path, "w")
+    path = laser.get_path()
+    laser.print_laserMaze()    
     if path[-1] != -1:
         fp.write("-1")
     else:
@@ -150,12 +137,12 @@ def writeFile(laser, filePath):
     fp.close()
     
 
-def runTest(inputFilePath, outputFilePath):
-    laser = readLaserMaze(inputFilePath)
-    writeFile(laser, outputFilePath)   
+def run_test(input_file_path, output_file_path):
+    laser = read_laserMaze(input_file_path)
+    write_file(laser, output_file_path)   
 
 
 
 if __name__ == '__main__':    
-    runTest(sys.argv[1], sys.argv[2])
+    run_test(sys.argv[1], sys.argv[2])
     
